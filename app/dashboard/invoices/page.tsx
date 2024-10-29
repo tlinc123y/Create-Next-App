45e7429292a9d950654b37af909e2fb0ff0f9996
Terry Lin <terry.lin@yourlibrary.ca>
2024-10-29 13:29:39 -0700

    Revert "make the search bar work in Ch. 11"
    This reverts commit 9c0bb064aba0747804ba055e581b921434f0a5c3.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,15 +5,8 @@ import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
-import { use } from "react";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: { query?: string; page?: string };
-}) {
-  const query = searchParams?.query || "";
-  const currentPage = Number(searchParams?.page || 1);
+export default async function Page() {
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -22,12 +15,6 @@ export default async function Page({
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
-        <Suspense
-          key={query + currentPage}
-          fallback={<InvoicesTableSkeleton />}
-        >
-          <Table query={query} currentPage={currentPage} />
-        </Suspense>
       </div>
       <div className="mt-5 flex w-full justify-center"></div>
     </div>
